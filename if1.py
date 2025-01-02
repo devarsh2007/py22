86524aa5a73f964b1a364c892943df78c8233fd7
devarsh2007 <sunilpatel2131@gmail.com>
2025-01-02 09:56:14 +0530

    condition

---
+++ if1.py
@@ -0,0 +1,8 @@
+age = int(input("enter your age : "))
+
+if 18<=age:
+    print("you are eligible")
+    
+print("good bye....")
+
+
---
+++ if2.py
@@ -0,0 +1,10 @@
+# find vowel
+# vowel => a,e,i,o,u
+# a => vowel
+
+data = input("enter a single character : ")
+
+if data=="a" or data=="i" or data=="e" or data=="o" or data=="u":
+    print(f"{data} is vowel")
+    
+print("good bye....")

20edb068fd4ee752e4bb2cc85a4d8ce12b252344
devarsh2007 <sunilpatel2131@gmail.com>
2025-01-20 10:03:26 +0530

    while

---
+++ while1.py
@@ -0,0 +1,19 @@
+# 1 2 3 .... 5
+number = 0
+last = int(input("enter number : "))
+
+while number<last:
+    number = number+1
+    print(number)
+
+# print(number)
+
+# number = number+1
+# print(number)
+
+# number = number+1
+# print(number)
+
+# number = number+1
+# print(number)
+
---
+++ while2.py
@@ -0,0 +1,21 @@
+# write a program that print symbol to n numbers
+# & - 5
+#  &&&&&
+# print(symbol*5)
+print("hello",end=" ")
+print("world",end=" ")
+print("dvcdhg")
+print("-----------------")
+
+symbol = input("enter a symbol : ")
+count = 1
+n = int(input("enter number : "))
+
+while count<=n:
+    print(symbol,end=" ")
+    count = count+1
+    
+# print(symbol)
+# print(symbol)
+# print(symbol)
+# print(symbol)

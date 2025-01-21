54f3a14325e01e2ff34861bd006ce9bb046feb62
devarsh2007 <sunilpatel2131@gmail.com>
2025-01-21 09:57:07 +0530

    while

---
+++ while3.py
@@ -0,0 +1,19 @@
+# 100 99 98 97...1
+number = int(input("enter a number : "))
+
+while number>=1:
+    print(number,end=" ")
+    number = number-1
+
+# print(number)
+
+# number = number-1
+# print(number)
+
+# number = number-1
+# print(number)
+# count=100
+# number=1
+# while count>=number :
+#     print(count )
+#     count=count -1
---
+++ while4.py
@@ -0,0 +1,84 @@
+gujarat_cities = [
+    "Ahmedabad",
+    "Surat",
+    "Vadodara",
+    "Rajkot",
+    "Bhavnagar",
+    "Jamnagar",
+    "Junagadh",
+    "Gandhinagar",
+    "Anand",
+    "Nadiad",
+    "Morbi",
+    "Bharuch",
+    "Navsari",
+    "Vapi",
+    "Porbandar",
+    "Dahod",
+    "Gandhidham",
+    "Mehsana",
+    "Palanpur",
+    "Veraval"
+]
+
+maharashtra_cities = [
+    "Mumbai",
+    "Pune",
+    "Nagpur",
+    "Nashik",
+    "Thane",
+    "Aurangabad",
+    "Solapur",
+    "Kolhapur",
+    "Amravati",
+    "Jalgaon"
+]
+
+rajasthan_cities = [
+    "Jaipur",
+    "Jodhpur",
+    "Udaipur",
+    "Kota",
+    "Ajmer",
+    "Bikaner",
+    "Alwar",
+    "Bhilwara",
+    "Sikar",
+    "Pali"
+]
+
+karnataka_cities = [
+    "Bengaluru",
+    "Mysuru",
+    "Mangaluru",
+    "Hubballi-Dharwad",
+    "Belagavi",
+    "Kalaburagi",
+    "Ballari",
+    "Davangere",
+    "Vijayapura",
+    "Shivamogga"
+]
+
+
+# print(len(gujarat_cities))
+
+count = 0
+# i = 0
+
+name = input("enter list name : ")
+
+while count < len(name):
+    print(name[count])
+    #increment
+    count+=1     # count = count+1 
+    # i+=1           # i = i+1
+    
+
+
+# if while loop not contain increment or decrement then infinite loop occurs
+    # count = count-1    #decrement
+
+# print(gujarat_cities[1])
+# print(gujarat_cities[2])
+# print(gujarat_cities[3])

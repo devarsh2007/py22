d8d171e738977ce9981e9b0c38fab382dcf78c01
devarsh2007 <sunilpatel2131@gmail.com>
2025-01-23 09:34:13 +0530

    for

--- for2.py
+++ for2.py
@@ -16,3 +16,6 @@ for i in range(0,len(name)):
 # print(name[3])
 # print(name[4])
 # print(name[5])
+print("------------------------------")
+for i in range(len(name)-1 , -1 , -1):
+    print(name[i])
---
+++ for3.py
@@ -0,0 +1,19 @@
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 10 = 50
+
+# print(number,"x",count,"=",ans)
+# count = 1
+
+number = int(input("enter a number : "))
+for i in range(1,11):
+    print(f"{number} x {i} = {number*i}")
+    # count+=1
+
+# ans = number*count
+# print(f"{number} x {count} = {ans}")
+
+# count+=1
+# ans = number*count
+# print(f"{number} x {count} = {ans}")

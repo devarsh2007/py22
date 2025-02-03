d792595e2ed1d504408104a5ef4cf70b0bf82a72
devarsh2007 <sunilpatel2131@gmail.com>
2025-02-03 10:24:11 +0530

    py

---
+++ function1.py
@@ -0,0 +1,27 @@
+# user define
+# function define
+def printline():
+    print("---------------------------------------------")
+    
+# printline() function call
+# printline()
+# printline()
+# print("function") 
+# printline()
+# printline()
+# printline()
+
+# 1-10
+# 1 x 1 =1
+def table():
+    start = int(input("enter starting number : "))
+    end = int(input("enter ending number : "))
+    # count=1
+    for j in range(start,end+1):
+        for i in range(1,11):
+            print(f"{j} x {i} = {j*i}")
+            
+        printline()
+        # count+=1
+        
+table()
---
+++ nestedloop2.py
@@ -0,0 +1,22 @@
+# 1
+# 12
+# 123
+# 1234
+
+# count = 1
+for j in range(1,5):
+    for i in range(1,j+1):
+        print(i,end=" ")
+    print("")
+
+# for i in range(0,2):
+#     print(count+i,end=" ")
+# print("")
+
+# for i in range(0,3):
+#     print(count+i,end=" ")
+# print("")
+
+# for i in range(0,4):
+#     print(count+i,end=" ")
+# print("hggf")
